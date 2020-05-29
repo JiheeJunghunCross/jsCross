e238fabb1e1e29fc1719ccdbb24022927ad10e32
JiheeSeon <starlover27@naver.com>
2020-05-30 05:12:59 +0900

    sanuengae himduelda

--- jscross/src/Components/Test.js
+++ jscross/src/Components/Test.js
@@ -1,3 +1,22 @@
 import React from "react";
+function Test() {
+  const name = "react";
+  const style = {
+    backgroundColor: "black",
+    color: "#ededed",
+    fontSize: 24,
+    margin: "10rem",
+  };
+  return (
+    <>
+      {" "}
+      {/*fragment*/}
+      <Hello />
+      <div style={style}>{name}</div>
+      <div className="a">hiii</div>
+      {/* ui component reuse */}
+    </>
+  );
+}
 
-const a = "jihee ulabula";
+export default Test;
